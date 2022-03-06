66e6d94f863967b540508f91392e800a3b1d5e53
Allan Kamimura <allan_kamimura@hotmail.com>
2022-03-06 19:02:33 -0300

    models name

--- signal/models/layers.py
+++ signal/models/layers.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-def preprocessing(input_shape):
+def MyInput(input_shape):
     input_signal = tf.keras.layers.Input(input_shape)    
     i = tf.keras.layers.Reshape((input_shape[:-1]))(input_signal)
 
